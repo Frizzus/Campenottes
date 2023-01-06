804ff6ab86fda06a58a370a697e2ad27f4d792a8
Frizzus <loiccanin.pro@gmail.com>
2023-01-06 13:16:26 +0100

    ajout hover sur le changement de langue

--- struct/fr/templates/header.php
+++ struct/fr/templates/header.php
@@ -1,10 +1,10 @@
 <header>
         <figure>
-            <img src="../../../img/netherlands-flag-icon.svg" alt="drapeau pays bas">
-            <img src="../../../img/germany-flag-icon.svg" alt="drapeau allemagne">
-            <img src="../../../img/italy-flag-icon.svg" alt="drapeau italie">
-            <img src="../../../img/united-kingdom-flag-icon.svg" alt="drapeau Grande Bretagne">
-            <img src="../../../img/france-flag-icon.svg" alt="drapeau france">
+            <a href="../../ne/pages/accueil.php"><img src="../../../img/netherlands-flag-icon.svg" alt="drapeau pays bas"></a>
+            <a href="../../de/pages/accueil.php"><img src="../../../img/germany-flag-icon.svg" alt="drapeau allemagne"></a>
+            <a href="../../it/pages/accueil.php"><img src="../../../img/italy-flag-icon.svg" alt="drapeau italie"></a>
+            <a href="../../en/pages/accueil.php"><img src="../../../img/united-kingdom-flag-icon.svg" alt="drapeau Grande Bretagne"></a>
+            <a href="../../fr/pages/accueil.php"><img src="../../../img/france-flag-icon.svg" alt="drapeau france"></a>
         </figure>
         <div id="title">
             <img src="../../../img/logo.png.png" alt="Logo des Campenottes">
