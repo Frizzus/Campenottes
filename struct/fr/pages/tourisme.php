c567b7e0367c2ab208346bac5f7db3319bf63c06
Frizzus <loiccanin.pro@gmail.com>
2023-01-06 14:11:10 +0100

    css tourisme mise en page aéré

--- struct/fr/pages/tourisme.php
+++ struct/fr/pages/tourisme.php
@@ -13,7 +13,7 @@
                 <p>La Haute-Savoie paradis pour les amoureux de la montagne que ce soit pour ses randonnées ou son histoire. De plus la région pourra vous proposer un voyage culinaire dont vous vous souviendrez.</p>
                 <p>Quoiqu'il en soit la maison propose une séléction des lieux touristiques à visiter ainsi qu'une carte interactive pour que vous puissiez prévoir votre itinéraire à travers notre belle région.</p>
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2753973.9651532774!2d1.2608883736195273!3d47.61781682698992!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478bf12817ecc0a9%3A0xa6ed84cdee99468!2sles%20campenottes!5e0!3m2!1sfr!2sfr!4v1666612775548!5m2!1sfr!2sfr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                <p>Voici une petite liste de nos recommandations: </p>
+                <h3>Voici une petite liste de nos recommandations: </h3>
                 <ul class="lastP">
                     <li>Annecy, la ville surnommée la Venise des Alpes. Jolie petite ville se trouvant entre un lac et une montagne.</li>
                     <li>Le Cairn, Restaurant gastronomique se trouvant en Saint Jean de Sixt même.</li>
